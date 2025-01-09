682fe3d42796f48a405c5f8e4bbdbec88c656641
johnathanbryce <johnathanbryce@gmail.com>
2025-01-09 10:58:00 -0800

    setup: basic GET & POST methods setup and tested

--- users/views.py
+++ users/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
 
-def index(request):
-    return HttpResponse("Hello from the Users app!")
+@api_view(['GET'])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)  #deserialize  to convert to Django Model Instance
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
